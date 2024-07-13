bf89be957c1475f8eb753aeb3103e7980299a7b7
cck <61312798+chaitanyakumar23@users.noreply.github.com>
2024-07-13 19:12:55 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -11,9 +11,10 @@ def main():
         sys.stdout.flush()
         sys.stdout.write("$ ")
         command = input()
-        sys.stdout.write(f"{command}: command not found\n")
         if command == 'exit 0':
             break
+        sys.stdout.write(f"{command}: command not found\n")
+
         
 
 if __name__ == "__main__":
