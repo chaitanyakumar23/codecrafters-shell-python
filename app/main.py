74e5ac176669e6799eba61d370b12dc3f2c20d57
cck <61312798+chaitanyakumar23@users.noreply.github.com>
2024-07-13 23:43:06 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ def main():
     # sys.stdout.write("$ ")
     # sys.stdout.flush()
 
+    builtins = ["echo", "exit", "type"]
+
     # Wait for user input
     while True:
         sys.stdout.flush()
@@ -17,6 +19,11 @@ def main():
                 sys.exit(0)
             elif args.startswith("echo "):
                 print(args[len("echo ") :])
+            elif args.startswith("type"):
+                if args[len("type ") :] in builtins:
+                    print(f'args[len("type ") :] is a shell builtin')
+                else:
+                    print(f'args[len("type ") :]: not found')
             else:
                 print(f"{args}: command not found")
         
