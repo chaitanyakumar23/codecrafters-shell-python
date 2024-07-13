09d0698ca14dcb18ba1a404a8a02c7192ac1cf62
cck <61312798+chaitanyakumar23@users.noreply.github.com>
2024-07-14 00:34:17 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -15,14 +15,14 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         args = command.strip()
+        cmd = args.split(" ")[1]
+        cmd_path = None
+        paths = PATH.split(":")        
         if args == "exit 0":
             sys.exit(0)
         elif args.startswith("echo "):
             print(args[len("echo ") :])
         elif args.startswith("type"):
-            cmd = args.split(" ")[1]
-            cmd_path = None
-            paths = PATH.split(":")
             for path in paths:
                 if os.path.isfile(f"{path}/{cmd}"):
                     cmd_path = f"{path}/{cmd}"
@@ -32,6 +32,8 @@ def main():
                 print(f"{cmd} is {cmd_path}")
             else:
                 print(f'{args[len("type ") :]}: not found')
+        elif os.path.isfile(args.split(" ")[0]):
+            os.system(args(args.split(" ")[1]))
         else:
             print(f"{args}: command not found")
         
