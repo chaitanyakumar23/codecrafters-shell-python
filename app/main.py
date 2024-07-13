6549f8790cc16849d8ba0f81041c743294f44e98
cck <61312798+chaitanyakumar23@users.noreply.github.com>
2024-07-13 18:49:23 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-    sys.stdout.flush()
+    # sys.stdout.flush()
 
     # Wait for user input
-    while True:
-        command = input()
+    while (command := input()) != "":
+        sys.stdout.flush()
         sys.stdout.write(f"{command}: command not found\n")
+        sys.stdout.write("$ ")
     
 if __name__ == "__main__":
     main()
